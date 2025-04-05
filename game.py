ac2e3f30d5663aab49d4c29b855e069d01ddb81a
NguyenDuy <ducduy1982005@gmail.com>
2025-04-05 14:22:35 +0700

    Added screen, rendered some chess pieces

--- game.py
+++ game.py
@@ -1,18 +1,30 @@
 import pygame
 from const import *
+from board import Board
 
 
 class Game:
     def __init__(self):
-        pass
+        self.board = Board()
 
     # Show methods
-    def show_background(selfself, screen):
+    def show_background(self, screen):
         for row in range(ROWS):
             for col in range(COLS):
                 if (row + col) % 2 == 0:
-                    color = (234, 235, 200)  # light green
+                    color = (235, 235, 208)  # light green
                 else:
-                    color = (119, 154, 88)  # dark green
+                    color = (119, 148, 85)  # dark green
                 rect = pygame.Rect(col * SQSIZE, row * SQSIZE, SQSIZE, SQSIZE)
                 pygame.draw.rect(screen, color, rect)
+
+    def show_pieces(self, screen):
+        for row in range(ROWS):
+            for col in range(COLS):
+                # has piece?
+                if self.board.squares[row][col].has_piece():
+                    piece = self.board.squares[row][col].piece
+                    img = pygame.image.load(piece.texture)
+                    img_center = col * SQSIZE + SQSIZE // 2, row * SQSIZE + SQSIZE // 2
+                    piece.texture_rect = img.get_rect(center=img_center)
+                    screen.blit(img, piece.texture_rect)
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
 from const import *
+from game import Game
 
 
 class Main:
@@ -8,17 +9,25 @@ class Main:
         pygame.init()
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
         pygame.display.set_caption('Chess_AI')
+        self.game = Game()
         pass
 
     def mainloop(self):
-
+        screen = self.screen
+        game = self.game
+        for squares in game.board.squares:
+            for square in squares:
+                print(square.has_piece(), end=" ")
+            print()
         while True:
-
+            game.show_background(screen)
+            game.show_pieces(screen)
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
             pygame.display.update()
+
 
 main = Main()
 main.mainloop()
